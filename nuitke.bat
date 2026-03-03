d5d1352b387a6e39f7dac7c4006dadd748870404
trees168 <censen01234@gmail.com>
2026-03-03 10:42:22 +0800

    加固打包脚本

--- nuitke.bat
+++ nuitke.bat
@@ -1,6 +1,16 @@
 @echo off
+setlocal enableextensions
+
 echo 开始打包...
+
+python -m pip install --upgrade nuitka
+
 python -m nuitka --standalone --show-progress --plugin-enable=tk-inter --output-dir=out ^
+	--assume-yes-for-downloads ^
+	--remove-output ^
+	--windows-console-mode=disable ^
+	--file-reference-choice=runtime ^
+	--lto=yes ^
 	--include-data-file=packages\install_config.json=packages\install_config.json ^
 	--include-data-file=packages\scripts\install_python.cmd=packages\scripts\install_python.cmd ^
 	--include-data-file=packages\scripts\install_mysql.cmd=packages\scripts\install_mysql.cmd ^
@@ -12,3 +22,5 @@ if exist out\WinPyDeploy.dist ren out\WinPyDeploy.dist WinPyDeploy
 if exist out\main.dist ren out\main.dist WinPyDeploy
 
 echo 打包完成！产物路径：out\WinPyDeploy
+
+endlocal
